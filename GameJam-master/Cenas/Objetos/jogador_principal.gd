3b60561cc7e96c47daca210d7e5e7892eec54612
rianflexa <rian.flexa@outlook.com>
2024-11-14 23:55:41 -0300

    fundo-parallax

--- GameJam-master/Cenas/Objetos/jogador_principal.gd
+++ GameJam-master/Cenas/Objetos/jogador_principal.gd
@@ -1,12 +1,20 @@
 extends CharacterBody2D
 
-
 const SPEED = 400.0
 const JUMP_VELOCITY = -900.0
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var lifepoints: Array[Node] = []
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var jump_count = 0
+var vida = 3
+
+func reduzir_vida():
+	vida -= 1
+	print(vida)
+	if (animated_sprite_2d.vida > 3):
+		
+		animated_sprite_2d.animation = "hit"
 
 func jump():
 	velocity.y = JUMP_VELOCITY
@@ -33,12 +41,13 @@ func _physics_process(delta: float) -> void:
 		else: 
 			animated_sprite_2d.animation = "jump"
 
-	# Handle jump.
+
+	# Detecta ação de pulo
 	if Input.is_action_just_pressed("jump") and jump_count < 2:
 		velocity.y = JUMP_VELOCITY
 		jump_count += 1
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
+	# Movimento horizontal
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
@@ -47,5 +56,6 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
-	var isLeft = velocity.x < 0
-	animated_sprite_2d.flip_h = isLeft
+	# Orienta o sprite baseado na direção
+	var is_left = velocity.x < 0
+	animated_sprite_2d.flip_h = is_left
